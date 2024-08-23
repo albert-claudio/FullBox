710f9791df81249fe5e72609e69f7527911e00d8
Albert Claudio <117605854+albert-claudio@users.noreply.github.com>
2024-08-23 08:55:14 -0300

    Update NewCollections.jsx

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection, setNew_collection] = useState([])
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://fullbox-node.onrender.com/newcollections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data))
   },[])
